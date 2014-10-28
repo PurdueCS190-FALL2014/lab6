cc8456b5eefc48338b9a56d5694385e2bc5bd6f9
Levi Starrett <levi@roxsoftware.com>
2014-10-28 18:10:40 -0400

    finished setup script

--- materials/setup.sh
+++ materials/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# change directories
-cd
-
 # clean
-rm -rf cs190lab6/
+rm -rf ~/cs190lab6/
+
+# change directories
+mkdir ~/cs190lab6/
+cd ~/cs190lab6/
 
 # get tarball
+wget https://raw.githubusercontent.com/PurdueCS190/lab6/master/materials/lab6.tgz
 
+# untar
+tar -zxvf lab6.tgz
+
+# remove tarball
+rm -f lab6.tgz
